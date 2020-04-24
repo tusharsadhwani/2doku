f40cf5aa19b2e09ef2448b022f880d5effc450a1
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-04-24 09:49:08 -0700

    added numbers

--- lib/screens/sudoku/sudoku_buttons.dart
+++ lib/screens/sudoku/sudoku_buttons.dart
@@ -6,9 +6,48 @@ class SudokuButtons extends StatelessWidget {
     return Center(
         child: Column(
       children: <Widget>[
-        Text("12345"),
-        Text("6789"),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SudokuButton(1),
+            SudokuButton(2),
+            SudokuButton(3),
+            SudokuButton(4),
+            SudokuButton(5),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SudokuButton(6),
+            SudokuButton(7),
+            SudokuButton(8),
+            SudokuButton(9),
+          ],
+        ),
       ],
     ));
+  }
+}
+
+class SudokuButton extends StatelessWidget {
+  final int value;
+  SudokuButton(this.value);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(2.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5.0),
+        color: Colors.grey.shade300,
+      ),
+      child: SizedBox(
+        width: 50,
+        height: 50,
+        child: FittedBox(
+          child: Text('$value'),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/sudoku/sudoku_grid.dart
+++ lib/screens/sudoku/sudoku_grid.dart
@@ -4,6 +4,28 @@ import 'package:flutter_svg/flutter_svg.dart';
 class SudokuGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SvgPicture.asset('assets/sudoku.svg');
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        SvgPicture.asset('assets/sudoku.svg'),
+        GridView.builder(
+          shrinkWrap: true,
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 9),
+          itemBuilder: (_, idx) => Container(
+            // color: idx % 2 == 0 ? Colors.red : Colors.blue,
+            child: FittedBox(
+              child: Text(
+                '${((idx ~/ 9) * 10 + idx % 9) % 9 + 1}',
+                style: TextStyle(
+                  color: idx % 2 == 0 ? Colors.black : Colors.blue,
+                ),
+              ),
+            ),
+          ),
+          itemCount: 81,
+        ),
+      ],
+    );
   }
 }
